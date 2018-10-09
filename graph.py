11cd721e5ec9a28095a87321ccfa06f3ecdbe869
Kuzmin Alexey <alexeykuzmin0@gmail.com>
2018-10-09 22:09:16 +0300

    Third commit

--- graph.py
+++ graph.py
@@ -1,3 +1,33 @@
+def f(ind):
+    return lex(ind) != "plus" and lex(ind) != "minus" and lex(ind) != "last" and lex(ind) != "close"
+
+
+def f1(ind):
+    return lex(ind) != "plus" and lex(ind) != "minus" and lex(ind) != "last" and lex(ind) != "close" and lex(ind) != "umn" and lex(ind) != "div"
+
+
+def f2(ind):
+    return ind == len(formula) or formula[ind] == "+" or formula[ind] == "-" or formula[ind] == "(" or formula[ind] == ")" or formula[ind] == "*" or formula[ind] == "/" or formula[ind] == "^" 
+
+
+def digit(ind):
+    if formula[ind].isdigit():
+        return 1
+    elif formula[ind] == "-" and (ind == 0 or formula[ind - 1] == "("):
+        return 1
+    return 0
+
+
+def cnt(ind):
+    res = ""
+    flg = 0
+    if formula[ind] == "-":
+        ind += 1
+        res += "-"
+    while not f2(ind):
+        res += formula[ind]
+        ind += 1
+    return res
 
 
 def lex(ind):
@@ -5,27 +35,26 @@ def lex(ind):
         return "last"
     if formula[ind] == "+":
         return "plus"
-    elif formula[ind] == "-":
+    elif formula[ind] == "-" and not digit(ind):
         return "minus"
     elif formula[ind] == "(":
         return "open"
     elif formula[ind] == ")":
         return "close"
+    elif formula[ind] == "*":
+        return "umn"
+    elif formula[ind] == "/":
+        return "div"
+    elif formula[ind] == "^":
+        return "step"
     else:
-        cnt = ""
-        while ind < len(formula) and formula[ind].isdigit():
-            cnt += formula[ind]
-            ind += 1
-        return cnt
-
+        return cnt(ind)
 
 
 def bracket(ind):
     value = lex(ind)
     if value == "last":
         return "l"
-    elif value.isdigit():
-        return int(value), len(value)
     elif value == "open":
         r = ind
         bal = 1
@@ -36,23 +65,56 @@ def bracket(ind):
             elif lex(r) == "close":
                 bal -= 1
         return plus(ind + 1, r), r - ind + 1
+    else:
+        return float(value), len(value)
 
 
+def st(ind):
+    res = 0
+    a = bracket(ind)
+    res = a[0]
+    ind += a[1]
+    while f1(ind):
+        if lex(ind) == "step":
+            a = bracket(ind + 1)
+            if a == "l":
+                break
+            res = res ** a[0]
+            ind += a[1] + 1
+    return res, ind
+
+
+def pro(ind):
+    res = 0
+    a = st(ind)
+    res = a[0]
+    ind = a[1]
+    while f(ind):
+        if lex(ind) == "umn":
+            a = st(ind + 1)
+            res *= a[0]
+            ind = a[1]
+        elif lex(ind) == "div":
+            a = st(ind + 1)
+            res /= a[0]
+            ind = a[1]
+    return res, ind
+    
 
 def plus(l, r):
     res = 0
-    a = bracket(l)
+    a = pro(l)
     res += a[0]
-    l += a[1]
+    l = a[1]
     while l < r:
         if lex(l) == "plus":
-            a = bracket(l + 1)
+            a = pro(l + 1)
             res += a[0]
-            l += a[1] + 1
+            l = a[1]
         elif lex(l) == "minus":
-            a = bracket(l + 1)
+            a = pro(l + 1)
             res -= a[0]
-            l += a[1] + 1
+            l = a[1]
     return res
 
 
@@ -60,7 +122,6 @@ def calc(fr):
     global formula
     formula = fr
     return plus(0, len(formula))
-
 
 fr = input()
 print(calc(fr))
