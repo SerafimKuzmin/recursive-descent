51572685fdef2182c7fc368bae229446e9cf5c78
Kuzmin Alexey <alexeykuzmin0@gmail.com>
2018-10-13 22:15:41 +0300

    Fourth commit

--- graph.py
+++ graph.py
@@ -1,16 +1,20 @@
-def f(ind):
-    return lex(ind) != "plus" and lex(ind) != "minus" and lex(ind) != "last" and lex(ind) != "close"
+from enum import Enum
 
 
-def f1(ind):
-    return lex(ind) != "plus" and lex(ind) != "minus" and lex(ind) != "last" and lex(ind) != "close" and lex(ind) != "umn" and lex(ind) != "div"
+class Sign(Enum):
+    minus = 1
+    plus = 2
+    mul = 3
+    div = 4
+    exp = 5
+    last = 6
+    op = 7
+    cl = 8
+    count = 9
 
 
-def f2(ind):
-    return ind == len(formula) or formula[ind] == "+" or formula[ind] == "-" or formula[ind] == "(" or formula[ind] == ")" or formula[ind] == "*" or formula[ind] == "/" or formula[ind] == "^" 
-
-
-def digit(ind):
+def digit():
+    global ind
     if formula[ind].isdigit():
         return 1
     elif formula[ind] == "-" and (ind == 0 or formula[ind - 1] == "("):
@@ -18,110 +22,92 @@ def digit(ind):
     return 0
 
 
-def cnt(ind):
+def cnt():
+    global ind
     res = ""
-    flg = 0
     if formula[ind] == "-":
         ind += 1
         res += "-"
-    while not f2(ind):
+    while ind < len(formula) and (formula[ind].isdigit() or formula[ind] == "."):
         res += formula[ind]
         ind += 1
+    return float(res)
+
+
+def lex():
+    global ind
+    if ind >= len(formula):
+        return Sign.last
+    if formula[ind] == "+":
+        return Sign.plus
+    elif formula[ind] == "-" and not digit():
+        return Sign.minus
+    elif formula[ind] == "(":
+        return Sign.op
+    elif formula[ind] == ")":
+        return Sign.cl
+    elif formula[ind] == "*":
+        return Sign.mul
+    elif formula[ind] == "/":
+        return Sign.div
+    elif formula[ind] == "^":
+        return Sign.exp
+    else:
+        return Sign.count
+
+
+
+def bracket():
+    global ind
+    if lex() == Sign.op:
+        ind += 1
+        res = addition()
+        ind += 1
+        return res
+    else:
+        return cnt()
+
+def exponentiation():
+    global ind
+    res = bracket()
+    while lex() == Sign.exp:
+        ind += 1
+        res **= bracket()
     return res
 
 
-def lex(ind):
-    if ind >= len(formula):
-        return "last"
-    if formula[ind] == "+":
-        return "plus"
-    elif formula[ind] == "-" and not digit(ind):
-        return "minus"
-    elif formula[ind] == "(":
-        return "open"
-    elif formula[ind] == ")":
-        return "close"
-    elif formula[ind] == "*":
-        return "umn"
-    elif formula[ind] == "/":
-        return "div"
-    elif formula[ind] == "^":
-        return "step"
-    else:
-        return cnt(ind)
-
-
-def bracket(ind):
-    value = lex(ind)
-    if value == "last":
-        return "l"
-    elif value == "open":
-        r = ind
-        bal = 1
-        while bal != 0:
-            r += 1
-            if lex(r) == "open":
-                bal += 1
-            elif lex(r) == "close":
-                bal -= 1
-        return plus(ind + 1, r), r - ind + 1
-    else:
-        return float(value), len(value)
-
-
-def st(ind):
-    res = 0
-    a = bracket(ind)
-    res = a[0]
-    ind += a[1]
-    while f1(ind):
-        if lex(ind) == "step":
-            a = bracket(ind + 1)
-            if a == "l":
-                break
-            res = res ** a[0]
-            ind += a[1] + 1
-    return res, ind
-
-
-def pro(ind):
-    res = 0
-    a = st(ind)
-    res = a[0]
-    ind = a[1]
-    while f(ind):
-        if lex(ind) == "umn":
-            a = st(ind + 1)
-            res *= a[0]
-            ind = a[1]
-        elif lex(ind) == "div":
-            a = st(ind + 1)
-            res /= a[0]
-            ind = a[1]
-    return res, ind
+def multiplication():
+    global ind
+    res = exponentiation()
+    while lex() == Sign.mul or lex() == Sign.div:
+        if lex() == Sign.mul:
+            ind += 1
+            res *= multiplication()
+        else:
+            ind += 1
+            res /= multiplication()
+    return res
     
 
-def plus(l, r):
-    res = 0
-    a = pro(l)
-    res += a[0]
-    l = a[1]
-    while l < r:
-        if lex(l) == "plus":
-            a = pro(l + 1)
-            res += a[0]
-            l = a[1]
-        elif lex(l) == "minus":
-            a = pro(l + 1)
-            res -= a[0]
-            l = a[1]
+def addition():
+    global ind
+    res = multiplication()
+    while lex() == Sign.plus or lex() == Sign.minus:
+        if lex() == Sign.plus:
+            ind += 1
+            res += multiplication()
+        else:
+            ind += 1
+            res -= multiplication()
     return res
 
 
 def calc(fr):
-    global formula
+    global formula, ind 
+    ind = 0
     formula = fr
-    return plus(0, len(formula))
+    return addition()
+
 
 fr = input()
 print(calc(fr))
