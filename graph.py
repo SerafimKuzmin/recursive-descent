cf9ad70b9466bd29c56faf25ffb71af2bcbd2722
Kuzmin Alexey <alexeykuzmin0@gmail.com>
2018-10-16 23:04:29 +0300

    Class

--- graph.py
+++ graph.py
@@ -13,102 +13,99 @@ class Sign(Enum):
     number = 9
 
 
-def digit():
-    global ind
-    if formula[ind].isdigit():
-        return 1
-    elif formula[ind] == "-" and (ind == 0 or formula[ind - 1] == "("):
-        return 1
-    return 0
+class Calculator:
+    def __init__(self, fr):
+        self.formula = fr
+        self.ind = 0
 
+        
+    def digit(self):
+        if self.formula[self.ind].isdigit():
+            return 1
+        elif self.formula[self.ind] == "-" and (self.ind == 0 or self.formula[self.ind - 1] == "("):
+            return 1
+        return 0
 
-def str_to_number():
-    global ind
-    res = ""
-    if formula[ind] == "-":
-        ind += 1
-        res += "-"
-    while formula[ind] != "$" and (formula[ind].isdigit() or formula[ind] == "."):
-        res += formula[ind]
-        ind += 1
-    return float(res)
+    
+    def str_to_number(self):
+        res = ""
+        if self.formula[self.ind] == "-":
+            self.ind += 1
+            res += "-"
+        while self.formula[self.ind] != "$" and (self.formula[self.ind].isdigit() or self.formula[self.ind] == "."):
+            res += self.formula[self.ind]
+            self.ind += 1
+        return float(res)
 
-
-def get_lexem():
-    global ind
-    if ind == "$":
-        return Sign.last
-    if formula[ind] == "+":
-        return Sign.plus
-    elif formula[ind] == "-" and not digit():
-        return Sign.minus
-    elif formula[ind] == "(":
-        return Sign.op
-    elif formula[ind] == ")":
-        return Sign.cl
-    elif formula[ind] == "*":
-        return Sign.mul
-    elif formula[ind] == "/":
-        return Sign.div
-    elif formula[ind] == "^":
-        return Sign.pw
-    else:
-        return Sign.number
-
-
-
-def element():
-    global ind
-    if get_lexem() == Sign.op:
-        ind += 1
-        res = addition()
-        ind += 1
-        return res
-    else:
-        return str_to_number()
-
-def exponentiation():
-    global ind
-    res = element()
-    while get_lexem() == Sign.pw:
-        ind += 1
-        res **= exponentiation()
-    return res
-
-
-def multiplication():
-    global ind
-    res = exponentiation()
-    while get_lexem() == Sign.mul or get_lexem() == Sign.div:
-        if get_lexem() == Sign.mul:
-            ind += 1
-            res *= multiplication()
+    
+    def get_lexem(self):
+        if self.ind == "$":
+            return Sign.last
+        if self.formula[self.ind] == "+":
+            return Sign.plus
+        elif self.formula[self.ind] == "-" and not self.digit():
+            return Sign.minus
+        elif self.formula[self.ind] == "(":
+            return Sign.op
+        elif self.formula[self.ind] == ")":
+            return Sign.cl
+        elif self.formula[self.ind] == "*":
+            return Sign.mul
+        elif self.formula[self.ind] == "/":
+            return Sign.div
+        elif self.formula[self.ind] == "^":
+            return Sign.pw
         else:
-            ind += 1
-            res /= multiplication()
-    return res
+            return Sign.number
+
+    
+    def element(self):
+        if self.get_lexem() == Sign.op:
+            self.ind += 1
+            res = self.addition()
+            self.ind += 1
+            return res
+        else:
+            return self.str_to_number()
+
+        
+    def exponentiation(self):
+        res = self.element()
+        while self.get_lexem() == Sign.pw:
+            self.ind += 1
+            res **= self.exponentiation()
+        return res
     
 
-def addition():
-    global ind
-    res = multiplication()
-    while get_lexem() == Sign.plus or get_lexem() == Sign.minus:
-        if get_lexem() == Sign.plus:
-            ind += 1
-            res += multiplication()
-        else:
-            ind += 1
-            res -= multiplication()
-    return res
+    def multiplication(self):
+        res = self.exponentiation()
+        while self.get_lexem() == Sign.mul or self.get_lexem() == Sign.div:
+            if self.get_lexem() == Sign.mul:
+                self.ind += 1
+                res *= self.multiplication()
+            else:
+                self.ind += 1
+                res /= self.multiplication()
+        return res
 
 
-def calc(fr):
-    global formula, ind 
-    ind = 0
-    formula = fr
-    formula += "$"
-    return addition()
+    def addition(self):
+        res = self.multiplication()
+        while self.get_lexem() == Sign.plus or self.get_lexem() == Sign.minus:
+            if self.get_lexem() == Sign.plus:
+                self.ind += 1
+                res += self.multiplication()
+            else:
+                self.ind += 1
+                res -= self.multiplication()
+        return res
+
+
+    def calc(self):
+        self.formula += "$"
+        return self.addition()
 
 
 fr = input()
-print(calc(fr))
+clcltr = Calculator(fr)
+print(clcltr.calc())
