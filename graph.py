be2fb83f0848988a9592d03a52ddf9111b7a9e24
Serafim Kuzmin <alexeykuzmin0@gmail.com>
2019-05-02 22:46:32 +0300

    Adding buttons

--- graph.py
+++ graph.py
@@ -24,20 +24,181 @@ class Calculator:
         self.ind = 0
         self.master = Tk()
         self.master.geometry("600x400")
-        self.button = Button(self.master, text = "deem", width = 10, height = 2,
-                        bg = "green", fg = "red", activebackground = "green",
-                        command = self.clicked)
-        self.button.place(x = 250,y = 25)
+        self.button1 = Button(self.master, text = "1", width = 3, height = 2, command = self.clicked_button1)
+        self.button1.place(x = 200, y = 25)
+        self.button2 = Button(self.master, text = "2", width = 3, height = 2, command = self.clicked_button2)
+        self.button2.place(x = 230, y = 25)
+        self.button3 = Button(self.master, text = "3", width = 3, height = 2, command = self.clicked_button3)
+        self.button3.place(x = 260, y = 25)
+        self.plus = Button(self.master, text = "+", width = 3, height = 2, command = self.clicked_plus)
+        self.plus.place(x = 290, y = 25)
+        self.button = Button(self.master, text = "=", width = 3, height = 2, command = self.clicked)
+        self.button.place(x = 320, y = 25)
+        self.dot = Button(self.master, text = "CE", width = 3, height = 2, command = self.clicked_del)
+        self.dot.place(x = 350, y = 25)
+        self.button4 = Button(self.master, text = "4", width = 3, height = 2, command = self.clicked_button4)
+        self.button4.place(x = 200, y = 60)
+        self.button5 = Button(self.master, text = "5", width = 3, height = 2, command = self.clicked_button5)
+        self.button5.place(x = 230, y = 60)
+        self.button6 = Button(self.master, text = "6", width = 3, height = 2, command = self.clicked_button6)
+        self.button6.place(x = 260, y = 60)
+        self.minus = Button(self.master, text = "-", width = 3, height = 2, command = self.clicked_minus)
+        self.minus.place(x = 290, y = 60)
+        self.parenthesis = Button(self.master, text = "(", width = 3, height = 2, command = self.clicked_parenthesis)
+        self.parenthesis.place(x = 320,y = 60)
+        self.parenthesis0 = Button(self.master, text = ")", width = 3, height = 2, command = self.clicked_parenthesis0)
+        self.parenthesis0.place(x = 350,y = 60)
+        self.button7 = Button(self.master, text = "7", width = 3, height = 2, command = self.clicked_button7)
+        self.button7.place(x = 200, y = 95)
+        self.button8 = Button(self.master, text = "8", width = 3, height = 2, command = self.clicked_button8)
+        self.button8.place(x = 230, y = 95)
+        self.button9 = Button(self.master, text = "9", width = 3, height = 2, command = self.clicked_button9)
+        self.button9.place(x = 260, y = 95)
+        self.multiply = Button(self.master, text = "*", width = 3, height = 2, command = self.clicked_multiply)
+        self.multiply.place(x = 290, y = 95)
+        self.button_sin = Button(self.master, text = "sin", width = 3, height = 2, command = self.clicked_sin)
+        self.button_sin.place(x = 320, y = 95)
+        self.button_cos = Button(self.master, text = "cos", width = 3, height = 2, command = self.clicked_cos)
+        self.button_cos.place(x = 350, y = 95)
+        self.dot = Button(self.master, text = ".", width = 3, height = 2, command = self.clicked_dot)
+        self.dot.place(x = 200, y = 130)
+        self.button0 = Button(self.master, text = "0", width = 3, height = 2, command = self.clicked_button0)
+        self.button0.place(x = 230, y = 130)
+        self.exp = Button(self.master, text = "^", width = 3, height = 2, command = self.clicked_exp)
+        self.exp.place(x = 260, y = 130)
+        self.div = Button(self.master, text = "/", width = 3, height = 2, command = self.clicked_div)
+        self.div.place(x = 290, y = 130)
+        self.button_tg = Button(self.master, text = "tg", width = 3, height = 2, command = self.clicked_tg)
+        self.button_tg.place(x = 320, y = 130)
+        self.button_ctg = Button(self.master, text = "ctg", width = 3, height = 2, command = self.clicked_ctg)
+        self.button_ctg.place(x = 350, y = 130)
+        self.button_asin = Button(self.master, text = "asin", width = 3, height = 2, command = self.clicked_arcsin)
+        self.button_asin.place(x = 200, y = 165)
+        self.button_acos = Button(self.master, text = "acos", width = 3, height = 2, command = self.clicked_arccos)
+        self.button_acos.place(x = 230, y = 165)
+        self.button_atg = Button(self.master, text = "atg", width = 3, height = 2, command = self.clicked_arctg)
+        self.button_atg.place(x = 260, y = 165)
+        self.button_actg = Button(self.master, text = "actg", width = 3, height = 2, command = self.clicked_arcctg)
+        self.button_actg.place(x = 290, y = 165)
+        self.button_arctg = Button(self.master, text = "atg", width = 3, height = 2, command = self.clicked_arctg)
+        self.button_arctg.place(x = 320, y = 165)
+        self.button_arcctg = Button(self.master, text = "actg", width = 3, height = 2, command = self.clicked_arcctg)
+        self.button_arcctg.place(x = 350, y = 165)
+        self.button_log2 = Button(self.master, text = "log2", width = 4, height = 2, command = self.clicked_log2)
+        self.button_log2.place(x = 200, y = 200)
+        self.button_log10 = Button(self.master, text = "log10", width = 4, height = 2, command = self.clicked_log10)
+        self.button_log10.place(x = 240, y = 200)
+        self.button_loge = Button(self.master, text = "loge", width = 4, height = 2, command = self.clicked_loge)
+        self.button_loge.place(x = 280, y = 200)
+        self.button_exp = Button(self.master, text = "exp", width = 3, height = 2, command = self.clicked_exponent)
+        self.button_exp.place(x = 320, y = 200)
+        self.button_sqrt = Button(self.master, text = "√", width = 3, height = 2, command = self.clicked_sqrt)
+        self.button_sqrt.place(x = 350, y = 200)
         self.entry = Entry(self.master)
-        self.entry.place(x = 250, y = 0)
-        self.label = Label(self.master, text = "")
-        self.label.place(x = 250, y = 60)
+        self.entry.place(x = 200, y = 0)
         self.master.mainloop()
 
 
+    def clicked_del(self):
+        self.entry.delete(len(self.entry.get()) - 1)
+
+    def clicked_log2(self):
+        self.entry.insert(END, "log2(")
+
+    def clicked_log10(self):
+        self.entry.insert(END, "log10(")
+
+    def clicked_loge(self):
+        self.entry.insert(END, "loge(")
+
+    def clicked_exponent(self):
+        self.entry.insert(END, "exp(")
+
+    def clicked_sqrt(self):
+        self.entry.insert(END, "sqrt(")
+        
+    def clicked_tg(self):
+        self.entry.insert(END, "tg(")
+
+    def clicked_ctg(self):
+        self.entry.insert(END, "ctg(")
+
+    def clicked_arctg(self):
+        self.entry.insert(END, "arctg(")
+
+    def clicked_arcctg(self):
+        self.entry.insert(END, "arcctg(")
+
+    def clicked_arcsin(self):
+        self.entry.insert(END, "arcsin(")
+
+    def clicked_arccos(self):
+        self.entry.insert(END, "arccos(")
+
+    def clicked_sin(self):
+        self.entry.insert(END, "sin(")
+
+    def clicked_cos(self):
+        self.entry.insert(END, "cos(")
+
+    def clicked_parenthesis(self):
+        self.entry.insert(END, "(")
+
+    def clicked_parenthesis0(self):
+        self.entry.insert(END, ")")
+
+    def clicked_exp(self):
+        self.entry.insert(END, "^")
+
+    def clicked_dot(self):
+        self.entry.insert(END, ".")
+
+    def clicked_multiply(self):
+        self.entry.insert(END, "*")
+
+    def clicked_div(self):
+        self.entry.insert(END, "/")
+
+    def clicked_minus(self):
+        self.entry.insert(END, "-")
+
+    def clicked_plus(self):
+        self.entry.insert(END, "+")
+
+    def clicked_button1(self):
+        self.entry.insert(END, "1")
+
+    def clicked_button2(self):
+        self.entry.insert(END, "2")
+
+    def clicked_button3(self):
+        self.entry.insert(END, "3")
+
+    def clicked_button4(self):
+        self.entry.insert(END, "4")
+
+    def clicked_button5(self):
+        self.entry.insert(END, "5")
+
+    def clicked_button6(self):
+        self.entry.insert(END, "6")
+
+    def clicked_button7(self):
+        self.entry.insert(END, "7")
+
+    def clicked_button8(self):
+        self.entry.insert(END, "8")
+
+    def clicked_button9(self):
+        self.entry.insert(END, "9")
+
+    def clicked_button0(self):
+        self.entry.insert(END, "0")
+
     def clicked(self):
         fr = self.entry.get()
-        self.label.configure(text = str(self.calc(fr)))
+        self.entry.delete(0, END)
+        self.entry.insert(0, str(self.calc(fr)))
 
         
     def digit(self):
@@ -74,7 +235,7 @@ class Calculator:
             self.ind += 1
         if name == "sin":
             res = self.element()
-            return math.sin(res)
+            return math.sin(math.radians(res))
         elif name == "cos":
             res = self.element()
             return math.cos(math.radians(res))
@@ -84,12 +245,6 @@ class Calculator:
         elif name == "ctg":
             res = self.element()
             return 1 / math.tan(math.radians(res))
-        elif name == "sec":
-            res = self.element()
-            return 1 / math.sin(math.radians(res))
-        elif name == "cosec":
-            res = self.element()
-            return 1 / math.cos(math.radians(res))
         elif name == "arcsin":
             res = self.element()
             return math.degrees(math.asin(res))
@@ -119,6 +274,7 @@ class Calculator:
         elif name == "sqrt":
             res = self.element()
             return math.sqrt(res)
+
     
     def get_lexem(self):
         if self.ind == "$":
@@ -192,21 +348,10 @@ class Calculator:
 
 
     def calc(self, fr):
-        flag = 0
         self.formula = fr
         self.formula += "$"
         self.ind = 0
-        for i in range(len(fr)):
-            self.ind = i
-            if self.get_lexem() == Sign.variable:
-                flag = 1
         d = dict()
-        if flag:
-            n = int(input("How many variables?\n"))
-            for i in range(n):
-                name, f = input().split(" = ")
-                d[name] = float(self.calc(f))
-                print(d[name])
         self.variables = d
         self.formula = fr
         self.formula += "$"
@@ -216,5 +361,3 @@ class Calculator:
 
 
 clcltr = Calculator()
-
-
